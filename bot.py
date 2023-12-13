8e82762504784022d2868cd9a16a02ae1c0ad93b
dhaan-ish <119067139+dhaan-ish@users.noreply.github.com>
2023-12-13 11:30:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 API_URL = "https://api-inference.huggingface.co/models/wizzseen/Chatbot-peter"
-headers = {"Authorization": "Bearer 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"}
+HEADERS = {"Authorization": f"Bearer {os.getenv('HF_API_KEY')}"}
+
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
+	response = requests.post(API_URL, headers=HEADERS, json=payload)
 	return response.json()
 	
 output=query("what is 911")
