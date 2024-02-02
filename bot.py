b503e577bf0ccce2f58955c2613993f4fa507fad
dhaan-ish <119067139+dhaan-ish@users.noreply.github.com>
2024-02-02 18:03:31 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-from gradio_client import Client
-
-client = Client("https://5642b7c4782b646128.gradio.live/")
-result = client.predict(
-		"Hello!!",	# str  in 'text' Textbox component
-							api_name="/predict"
-)
-print(result)
